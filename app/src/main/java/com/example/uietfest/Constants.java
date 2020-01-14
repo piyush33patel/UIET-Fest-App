c07f35623295c9e90e9aa5518a4ab7d0b1b54138
piyush33patel <56225984+piyush33patel@users.noreply.github.com>
2020-01-15 01:59:43 +0530

    Update Constants.java

--- app/src/main/java/com/example/uietfest/Constants.java
+++ app/src/main/java/com/example/uietfest/Constants.java
@@ -1,8 +1,14 @@
 package com.example.uietfest;
 
 public class Constants {
-    private static final String ROOT_URL = "https://websiteasofjunetwelve.000webhostapp.com/";
+    private static final String ROOT_URL = "/";             /*Website Unavailable*/
     public static final String URL_REGISTER = ROOT_URL + "UIETApp/RegisterUser.php";
     public static final String URL_INFO = ROOT_URL + "UIETApp/FetchUserInfo.php";
-
+    public static final String URL_PARTICIPATION_LIST = ROOT_URL + "UIETApp/YourParticipationList.php";
+    public static final String URL_DELETE_PARTICIPATION = ROOT_URL + "UIETApp/DeleteParticipation.php";
+    public static final String URL_FETCH_CULTURAL = ROOT_URL + "UIETApp/FetchCultural.php";
+    public static final String URL_FETCH_SPORTS = ROOT_URL + "UIETApp/FetchSports.php";
+    public static final String URL_FETCH_TECHNICAL = ROOT_URL + "UIETApp/FetchTechnical.php";
+    public static final String URL_SHOW_DIALOG = ROOT_URL + "UIETApp/ShowDialog.php";
+    public static final String URL_PARTICIPTE_STUDENT = ROOT_URL + "UIETApp/ParticipateStudent.php";
 }
